6c3235baa5aa2f99af06b4fb1fc838514ce66fa7
Brandon Olin <brandolomite@gmail.com>
2015-08-21 10:46:06 -0700

    Bugfixes

--- PasswordState.psm1
+++ PasswordState.psm1
@@ -1,12 +1,10 @@
-﻿Set-StrictMode -Version 3
+﻿$moduleRoot = Split-Path -Path $MyInvocation.MyCommand.Path
 
-# Load functions
-$moduleRoot = Split-Path -Path $MyInvocation.MyCommand.Path
 "$moduleRoot\Functions\*.ps1", "$moduleRoot\Internal\*.ps1" |
-Resolve-Path |
-ForEach-Object { . $_.ProviderPath }
+    Resolve-Path |
+    ForEach-Object { . $_.ProviderPath }
 
 # Allow untrusted SSL
 _SetCertPolicy
 
-Export-ModuleMember -Cmdlet *PasswordState*
+Export-ModuleMember -Function *PasswordState*
