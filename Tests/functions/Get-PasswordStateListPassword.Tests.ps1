23750270fdbb1588ee44f7a43d7d07fd23d818a3
Tom Robijns <Tom.Robijns@realdolmen.com>
2020-01-01 21:59:52 +0100

    Add Unit tests for Get-PasswordStateListPassword

--- Tests/functions/Get-PasswordStateListPassword.Tests.ps1
+++ Tests/functions/Get-PasswordStateListPassword.Tests.ps1
@@ -1,6 +1,10 @@
-﻿
-#Remove-Module -Name 'PasswordState' -Force
-#Import-Module "$PSScriptRoot\..\..\PasswordState\PasswordState.psd1"
+﻿if (!(get-module Passwordstate)) {
+    Remove-Module -Name 'PasswordState' -Force
+    Import-Module "$PSScriptRoot\..\..\PasswordState\PasswordState.psd1"
+}
+if (!($global:TestJson) -or (!($global:TestXML))) {
+    . .\Testdata.ps1
+}
 InModuleScope 'PasswordState' {
     Describe "Function Get-PasswordStateListPassword" {
         BeforeAll {
@@ -12,6 +16,25 @@ InModuleScope 'PasswordState' {
                 ,@{param='UseV6Api';mandatory='False'}
                 ,@{param='ExcludePasswords';mandatory='False'}
             )
+            $ValidParameterFormatTests=@(
+                @{value='json';works=$True}
+                @{value='xml';works=$True}
+            )
+            $AllParameterFormatTests =$ValidParameterFormatTests+@{value='incorrecttype';works=$False}
+            $APIKey=@{
+                'GoodKey'=([pscredential]::new('A very good key indeed',(ConvertTo-SecureString -AsPlainText -Force -String 'Please continue, Jarvis')))
+                'BadKey'=([pscredential]::new('bad key, very bad key',(ConvertTo-SecureString -AsPlainText -Force -String 'Going to sleep now')))
+            }
+            Mock -CommandName '_GetDefault' -MockWith {
+                'https://IDoNotExists.com'
+            } -ParameterFilter { $Option -and $Option -eq 'api_endpoint'}
+
+            Mock -CommandName 'Invoke-RestMethod' -MockWith {
+                $global:TestJson['PasswordStateAllPasswordResponse'] | ConvertFrom-Json
+            } -ParameterFilter { $uri -and $uri -match '/passwords/' -and $ContentType -match 'json'}
+            Mock -CommandName 'Invoke-RestMethod' -MockWith {
+                [xml]($global:Testxml['PasswordStateAllPasswordResponse'])
+            } -ParameterFilter { $uri -and $uri -match '/passwords/' -and $ContentType -match 'xml'}
         }
         Context 'Analyzing Parameters' {
             It 'Should ensure parameter <param> is declared'-TestCases $ParameterTests {
@@ -21,6 +44,16 @@ InModuleScope 'PasswordState' {
             It "Should ensure that the mandatory property for Parameter <param> is set to <mandatory>" -TestCases $ParameterTests {
                 param($param,$mandatory)
                 "$((((Get-Command -Name 'Get-PasswordStateListPassword').Parameters[$param].Attributes | ? { $_.GetType().fullname -match 'ParameterAttribute'}) | select-object -first 1).Mandatory)" | should -be $mandatory
+            }
+            It "Should ensure that the format value <value> is accepted" -TestCases $AllParameterFormatTests {
+                param($value,$works)
+                $value -in ((get-command 'Get-PasswordStateListPassword').Parameters['Format'].Attributes | Where-Object { $_.gettype().fullname -eq 'System.Management.Automation.ValidateSetAttribute' }).ValidValues | should be $works
+            }
+        }
+        Context 'Unit Tests' {
+            It "Should return an object with an property 'totalpasswords' for <value> Format" -TestCases $ValidParameterFormatTests {
+                param($value, $works)
+                (Get-PasswordStateListPassword -ApiKey $APIKey['GoodKey'] -PasswordListId 1 -format $value).totalpasswords | should -not -be $null
             }
         }
     }
